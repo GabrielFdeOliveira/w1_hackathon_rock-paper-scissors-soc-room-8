0de924348fa9e724491996b948276a894166aae5
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2022-09-30 12:12:07 +0100

    added a function using our previous logic

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
 console.log("Connected")
 // Task 1 (Logic) plan
-// Create two variables to store the moves (our move, computer move)
-// Create an object with three properties insite, being rock, 
-//paper and scizor. Doing that for player and computer.
 
-
-// Create if statemets to determine the winner
-
+// Create two variables to store the moves (player move, computer move)
 let playerMove = "rock"
 let computerMove = "rock"
 
+// Create if statemets to determine the winner
+// Create a function using the logice we just created
+// The varieables that we just hard coded will now be taken as parameters of the function.
 
-if(playerMove === computerMove){console.log("It's a draw!")
-}
-
-if (playerMove === "rock" && computerMove === "paper" ){ 
-    console.log("You've lost!")}
-    
-
-if (playerMove === "rock" && computerMove === "scissors"){
-    console.log("You won!")}
-        
-if (playerMove === "scissors" && computerMove === "rock"){
-            console.log("You've lost!")}
-       
-if (playerMove === "scissors" && computerMove === "paper"){
-            console.log("You win!")}
-        
-if (playerMove === "paper" && computerMove === "rock"){
-                console.log("You win!")}
-
-if (playerMove === "paper" && computerMove === "scissors"){
-                console.log("You've lost!")} 
-    
+function getWinner(playerMove, computerMove){
+    if(playerMove === computerMove){
+        return 0;
+    }
+        if (playerMove === "rock" && computerMove === "paper" ){ 
+            return -1;
+        }      
+            if (playerMove === "rock" && computerMove === "scissors"){
+                return 1;
+            }                
+                if (playerMove === "scissors" && computerMove === "rock"){
+                    return -1;
+                }                
+                    if (playerMove === "scissors" && computerMove === "paper"){
+                        return 1;
+                    }                        
+                        if (playerMove === "paper" && computerMove === "rock"){
+                            return 1;
+                        }
+                            if (playerMove === "paper" && computerMove === "scissors"){
+                                return -1;
+                            } else {
+                                console.log("Choose between rock, scissors or paper")
+                            }                       
+}                    
+let result = getWinner()
+            
