2148f010da33843f0525247886025185dcb9f28c
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2022-09-30 11:06:30 +0100

    Started the logic on rock, paper, scissors game

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("Connected")
+// Task 1 (Logic) plan
+// Create two variables to store the moves (our move, computer move)
+// Create an object with three properties insite, being rock, 
+//paper and scizor. Doing that for player and computer.
+
+let playerMove;
+let computerMove; 
+
+
+if(playerMove === computerMove){
+    console.log("It's a draw!")
+}
+    if (playerMove = "rock", computerMove = "paper" ){
+        console.log("You've lost!")  
+    }else {
+        if (playerMove = "rock", computerMove = "scissors"){
+            console.log("You won!")
+        }else {
+            
+        }
+    }
+    
+
+
+// Create if statemets to determine the winner
+
+
+ 
