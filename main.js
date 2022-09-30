2a6dd295efc129184acc66a9dce5b199e8fcc877
Victoria Lampard <vglampard@protonmail.com>
2022-09-30 11:51:33 +0100

    Created if statements

--- main.js
+++ main.js
@@ -4,26 +4,32 @@ console.log("Connected")
 // Create an object with three properties insite, being rock, 
 //paper and scizor. Doing that for player and computer.
 
-let playerMove;
-let computerMove; 
-
-
-if(playerMove === computerMove){
-    console.log("It's a draw!")
-}
-    if (playerMove = "rock", computerMove = "paper" ){
-        console.log("You've lost!")  
-    }else {
-        if (playerMove = "rock", computerMove = "scissors"){
-            console.log("You won!")
-        }else {
-            
-        }
-    }
-    
-
 
 // Create if statemets to determine the winner
 
+let playerMove = "rock"
+let computerMove = "rock"
 
- 
+
+if(playerMove === computerMove){console.log("It's a draw!")
+}
+
+if (playerMove === "rock" && computerMove === "paper" ){ 
+    console.log("You've lost!")}
+    
+
+if (playerMove === "rock" && computerMove === "scissors"){
+    console.log("You won!")}
+        
+if (playerMove === "scissors" && computerMove === "rock"){
+            console.log("You've lost!")}
+       
+if (playerMove === "scissors" && computerMove === "paper"){
+            console.log("You win!")}
+        
+if (playerMove === "paper" && computerMove === "rock"){
+                console.log("You win!")}
+
+if (playerMove === "paper" && computerMove === "scissors"){
+                console.log("You've lost!")} 
+    
